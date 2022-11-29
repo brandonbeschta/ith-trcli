7dbe1c4e0b44e95f54a034be716f655e0c8d6d5f
Brandon <brandon.beschta@teladochealth.com>
2022-11-29 11:32:55 -0600

    Try something

--- trcli/data_classes/dataclass_testrail.py
+++ trcli/data_classes/dataclass_testrail.py
@@ -48,7 +48,7 @@ class TestRailResult:
         if test_result_tag == "skipped":
             return 4
         elif test_result_tag == "error" or "failure":
-            return 5
+            return 1
         elif test_result_tag == "autoPass":
             return 6
 
